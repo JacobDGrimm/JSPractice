7448eb511876c66b13bb92973ebc8413913e22e4
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-12-05 22:24:51 -0500

    adds a triangle area calculator

--- JavaScriptPractice/js/main.js
+++ JavaScriptPractice/js/main.js
@@ -1,1 +1,15 @@
-﻿// Your code here!
+﻿// Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7
+
+var sideA = 5;
+var sideB = 6;
+var sideC = 7;
+
+// herrons formula is area = square root of the semiperimeter times the semiperimeter minus each side
+
+var semi = (sideA + sideB + sideC) / 2;
+
+var area;
+
+area = Math.sqrt(semi*((semi - sideA)*(semi - sideB)*(semi - sideC)));
+
+console.log(area);
