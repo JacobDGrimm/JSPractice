1a7825bf52ef7a8ddf82e962a1cc69c13ed8f51a
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-12-07 21:37:11 -0500

    adds a problem of clocks

--- JavaScriptPractice/js/main.js
+++ JavaScriptPractice/js/main.js
@@ -13,3 +13,20 @@ var area;
 area = Math.sqrt(semi*((semi - sideA)*(semi - sideB)*(semi - sideC)));
 
 console.log(area);
+
+
+// find the angle between the hands of a clock at a specific time. I had this one in an interview but i want to try it in javascript now
+
+// step one is convert the hour and minute into angles on a circle
+// lets use 3:25
+// 60 minutes in an hour 360 / 60 = 6 so every minutes is six degrees
+// there are 360 degrees in a circle 12 hours on the clock. 360 / 12 
+var hour = 3;
+var minute = 25;
+var minuteAngle = minute * 6;
+var hourAngle = (hour * 30) + ((minute / 12) * 6);
+var answerAngle = Math.abs(minuteAngle - hourAngle);
+if (answerAngle > 180) {
+    answerAngle -= 180
+}
+console.log(answerAngle);
